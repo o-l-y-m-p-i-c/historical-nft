f730927c45dbe50c919c6dec0d4a4efb844f3c57
o-l-y-m-p-i-c <73851045+o-l-y-m-p-i-c@users.noreply.github.com>
2022-04-07 16:37:32 +0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,11 @@
 function snadwich(e){
     e.parentNode.classList.toggle("active")
 }
+let links = document.querySelectorAll(".navigation__link")
+
+for (let i = 0; i < links.length; i++) {
+    const element = links[i];
+    element.onclick = () => {
+        document.querySelector(".navigation__wrapper").classList.remove("active")
+    }
+}
